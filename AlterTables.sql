dbb6c33e990400bfba7c6d537a9a871a320db383
hronchetti <harryronchetti@gmail.com>
2018-01-29 18:11:02 +0000

    Adding on Delete Cascade for all foreign keys

--- AlterTables.sql
+++ AlterTables.sql
@@ -1,44 +1,44 @@
 /*    USER TABLES    */
 
 ALTER TABLE nf_users 
-ADD FOREIGN KEY (usertypeid) REFERENCES nf_usertypes(usertypeid),
+ADD FOREIGN KEY (usertypeid) REFERENCES nf_usertypes(usertypeid) ON DELETE CASCADE,
 ADD UNIQUE (username);
 
 ALTER TABLE nf_userspecialisms
-ADD FOREIGN KEY (userid) REFERENCES nf_users(userid),
-ADD FOREIGN KEY (specialismid) REFERENCES nf_specialismtypes(specialismid);
+ADD FOREIGN KEY (userid) REFERENCES nf_users(userid) ON DELETE CASCADE,
+ADD FOREIGN KEY (specialismid) REFERENCES nf_specialismtypes(specialismid) ON DELETE CASCADE;
 
 /*    PROJECT TABLES    */
 
 ALTER TABLE nf_projects
-ADD FOREIGN KEY (nerdid) REFERENCES nf_users(userid),
-ADD FOREIGN KEY (clientid) REFERENCES nf_users(userid),
-ADD FOREIGN KEY (specialismid) REFERENCES nf_specialismtypes(specialismid);
+ADD FOREIGN KEY (nerdid) REFERENCES nf_users(userid) ON DELETE CASCADE,
+ADD FOREIGN KEY (clientid) REFERENCES nf_users(userid) ON DELETE CASCADE,
+ADD FOREIGN KEY (specialismid) REFERENCES nf_specialismtypes(specialismid) ON DELETE CASCADE;
 
 /*    EVENT TABLES    */
 
 ALTER TABLE nf_events
-ADD FOREIGN KEY (creatorid) REFERENCES nf_users(userid);
+ADD FOREIGN KEY (creatorid) REFERENCES nf_users(userid) ON DELETE CASCADE;
 
 ALTER TABLE nf_eventattendance
-ADD FOREIGN KEY (userid) REFERENCES nf_users(userid),
-ADD FOREIGN KEY (eventid) REFERENCES nf_events(eventid);
+ADD FOREIGN KEY (userid) REFERENCES nf_users(userid) ON DELETE CASCADE,
+ADD FOREIGN KEY (eventid) REFERENCES nf_events(eventid) ON DELETE CASCADE;
 
 /*    COMMUNICATION TABLES    */
 
 ALTER TABLE nf_posts
-ADD FOREIGN KEY (userid) REFERENCES nf_users(userid),
-ADD FOREIGN KEY (forumid) REFERENCES nf_forums(forumid);
+ADD FOREIGN KEY (userid) REFERENCES nf_users(userid) ON DELETE CASCADE,
+ADD FOREIGN KEY (forumid) REFERENCES nf_forums(forumid) ON DELETE CASCADE;
 
 ALTER TABLE nf_comments
-ADD FOREIGN KEY (postid) REFERENCES nf_posts(postid),
-ADD FOREIGN KEY (userid) REFERENCES nf_users(userid);
+ADD FOREIGN KEY (postid) REFERENCES nf_posts(postid) ON DELETE CASCADE,
+ADD FOREIGN KEY (userid) REFERENCES nf_users(userid) ON DELETE CASCADE;
 
 ALTER TABLE nf_messages
-ADD FOREIGN KEY (senderid) REFERENCES nf_users(userid),
-ADD FOREIGN KEY (recieverid) REFERENCES nf_users(userid);
+ADD FOREIGN KEY (senderid) REFERENCES nf_users(userid) ON DELETE CASCADE,
+ADD FOREIGN KEY (recieverid) REFERENCES nf_users(userid) ON DELETE CASCADE;
 
 /*    ADMIN TABLES    */
 
 ALTER TABLE nf_techissues
-ADD FOREIGN KEY (reporterid) REFERENCES nf_users(userid);
+ADD FOREIGN KEY (reporterid) REFERENCES nf_users(userid) ON DELETE CASCADE;
